f6f8ccb9b7a25d817d5742069cd67851e1f20034
Familia Díaz <Familia Daz@DESKTOP-K0V8RO8>
2022-04-17 17:48:10 -0300

    asdfasdfasdf

---
+++ prueba/src/test/java/ar/edu/unju/fi/proyecto2/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.proyecto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
